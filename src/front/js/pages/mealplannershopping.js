f7ad323b574a140908e3f8068a3373498dbedb50
michellebessa <michelleandressa12@gmail.com>
2022-12-02 23:33:28 +0000

    commit

--- src/front/js/pages/mealplannershopping.js
+++ src/front/js/pages/mealplannershopping.js
@@ -47,7 +47,7 @@ export const MealPlannerShopping = () => {
       setFruits([...fruits, fruitsInput]);
     }
     //Here are the handleSubmit for the weekly meals accordion
-    if (meal == "breakfast" && breakfastInput) {
+    else if (meal == "breakfast" && breakfastInput) {
       setBreakfast([...breakfast, breakfastInput]);
       setBreakfastInput("");
     } else if (meal == "lunch" && lunchInput) {
@@ -80,178 +80,177 @@ export const MealPlannerShopping = () => {
       setFruits([...fruits, fruitsInput]);
       setFruitsInput("");
     }
-
-    return (
-      <div className="mt-5">
-        <div className="row">
-          <div className="col">
-            <li className="list-group-item" id="mealplannershopping">
-              <h1>Daily Meal Planner</h1>
-              <p>
-                <div className="dropdown">
-                  <button
-                    className="btn btn-secondary dropdown-toggle"
-                    type="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Select Week Day
-                  </button>
-                  <ul className="dropdown-menu">
-                    <WeekDay day="Monday" />
-                    <WeekDay day="Tuesday" />
-                    <WeekDay day="Wednesday" />
-                    <WeekDay day="Thursday" />
-                    <WeekDay day="Friday" />
-                    <WeekDay day="Saturday" />
-                    <WeekDay day="Sunday" />
-                  </ul>
-                </div>
-              </p>
-              <div className="accordion" id="accordionPanelsStayOpenExample">
-                <DailyMealPlanning mealid="Breakfast">
-                  <form
-                    onSubmit={(event) => {
-                      event.preventDefault();
-                      handleSubmit(event, "breakfast");
-                    }}
-                  >
-                    <input
-                      className="inputmealplanner"
-                      placeholder="What are you having for breakfast?"
-                      value={breakfastInput}
-                      onChange={(event) => {
-                        setBreakfastInput(event.target.value);
-                      }}
-                    ></input>
-                    {/*this button is for sending an item to the array list */}
-                    {/* <button onClick={handleSubmit("breakfast")}>Test</button> */}
-                  </form>
-                  {breakfast.map((meal) => {
-                    return (
-                      <ul className="m-0">
-                        <li id="mealmap">{meal}</li>
-                      </ul>
-                    );
-                  })}
-                </DailyMealPlanning>
-                <DailyMealPlanning mealid="Lunch">
-                  <form
-                    onSubmit={(event) => {
-                      event.preventDefault();
-                      handleSubmit(event, "lunch");
-                    }}
-                  >
-                    <input
-                      className="inputmealplanner"
-                      placeholder="What are you having for lunch?"
-                      value={lunchInput}
-                      onChange={(event) => {
-                        setLunchInput(event.target.value);
-                      }}
-                    ></input>
-                  </form>
-                  {lunch.map((meal) => {
-                    return (
-                      <ul className="m-0">
-                        <li id="mealmap">{meal}</li>
-                      </ul>
-                    );
-                  })}
-                </DailyMealPlanning>
-                <DailyMealPlanning mealid="Dinner">
-                  <form
-                    onSubmit={(event) => {
-                      event.preventDefault();
-                      handleSubmit(event, "dinner");
-                    }}
-                  >
-                    <input
-                      className="inputmealplanner"
-                      placeholder="What are you having for dinner?"
-                      value={dinnerInput}
-                      onChange={(event) => {
-                        setDinnerInput(event.target.value);
-                      }}
-                    ></input>
-                  </form>
-                  {dinner.map((meal) => {
-                    return (
-                      <ul className="m-0">
-                        <li id="mealmap">{meal}</li>
-                      </ul>
-                    );
-                  })}
-                </DailyMealPlanning>
+  };
+  return (
+    <div className="mt-5">
+      <div className="row">
+        <div className="col">
+          <li className="list-group-item" id="mealplannershopping">
+            <h1>Daily Meal Planner</h1>
+            <p>
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Select Week Day
+                </button>
+                <ul className="dropdown-menu">
+                  <WeekDay day="Monday" />
+                  <WeekDay day="Tuesday" />
+                  <WeekDay day="Wednesday" />
+                  <WeekDay day="Thursday" />
+                  <WeekDay day="Friday" />
+                  <WeekDay day="Saturday" />
+                  <WeekDay day="Sunday" />
+                </ul>
               </div>
-            </li>
-          </div>
-          <div className="col">
-            <li className="list-group-item" id="mealplannershopping">
-              <h1>Shopping List</h1>
-              <p>
-                <div className="dropdown">
-                  <button
-                    className="btn btn-secondary dropdown-toggle"
-                    type="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Select Week Day
-                  </button>
-                  <ul className="dropdown-menu">
-                    <WeekDay day="Monday" />
-                    <WeekDay day="Tuesday" />
-                    <WeekDay day="Wednesday" />
-                    <WeekDay day="Thursday" />
-                    <WeekDay day="Friday" />
-                    <WeekDay day="Saturday" />
-                    <WeekDay day="Sunday" />
-                  </ul>
-                </div>
-              </p>
-              <div className="accordion" id="accordionPanelsStayOpenExample">
-                <DailyMealPlanning mealid="Healthy-Fat">
+            </p>
+            <div className="accordion" id="accordionPanelsStayOpenExample">
+              <DailyMealPlanning mealid="Breakfast">
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault();
+                    handleSubmit(event, "breakfast");
+                  }}
+                >
                   <input
                     className="inputmealplanner"
-                    placeholder="Add an item to your list"
+                    placeholder="What are you having for breakfast?"
+                    value={breakfastInput}
+                    onChange={(event) => {
+                      setBreakfastInput(event.target.value);
+                    }}
                   ></input>
-                </DailyMealPlanning>
-                <DailyMealPlanning mealid="Protein">
+                  {/*this button is for sending an item to the array list */}
+                  {/* <button onClick={handleSubmit("breakfast")}>Test</button> */}
+                </form>
+                {breakfast.map((meal) => {
+                  return (
+                    <ul className="m-0">
+                      <li id="mealmap">{meal}</li>
+                    </ul>
+                  );
+                })}
+              </DailyMealPlanning>
+              <DailyMealPlanning mealid="Lunch">
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault();
+                    handleSubmit(event, "lunch");
+                  }}
+                >
                   <input
                     className="inputmealplanner"
-                    placeholder="Add an item to your list"
+                    placeholder="What are you having for lunch?"
+                    value={lunchInput}
+                    onChange={(event) => {
+                      setLunchInput(event.target.value);
+                    }}
                   ></input>
-                </DailyMealPlanning>
-                <DailyMealPlanning mealid="Dairy">
+                </form>
+                {lunch.map((meal) => {
+                  return (
+                    <ul className="m-0">
+                      <li id="mealmap">{meal}</li>
+                    </ul>
+                  );
+                })}
+              </DailyMealPlanning>
+              <DailyMealPlanning mealid="Dinner">
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault();
+                    handleSubmit(event, "dinner");
+                  }}
+                >
                   <input
                     className="inputmealplanner"
-                    placeholder="Add an item to your list"
+                    placeholder="What are you having for dinner?"
+                    value={dinnerInput}
+                    onChange={(event) => {
+                      setDinnerInput(event.target.value);
+                    }}
                   ></input>
-                </DailyMealPlanning>
-                <DailyMealPlanning mealid="Grains-Carbohydrates">
-                  <input
-                    className="inputmealplanner"
-                    placeholder="Add an item to your list"
-                  ></input>
-                </DailyMealPlanning>
-                <DailyMealPlanning mealid="Vegetables">
-                  <input
-                    className="inputmealplanner"
-                    placeholder="Add an item to your list"
-                  ></input>
-                </DailyMealPlanning>
-                <DailyMealPlanning mealid="Fruits">
-                  <input
-                    className="inputmealplanner"
-                    placeholder="Add an item to your list"
-                  ></input>
-                </DailyMealPlanning>
+                </form>
+                {dinner.map((meal) => {
+                  return (
+                    <ul className="m-0">
+                      <li id="mealmap">{meal}</li>
+                    </ul>
+                  );
+                })}
+              </DailyMealPlanning>
+            </div>
+          </li>
+        </div>
+        <div className="col">
+          <li className="list-group-item" id="mealplannershopping">
+            <h1>Shopping List</h1>
+            <p>
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Select Week Day
+                </button>
+                <ul className="dropdown-menu">
+                  <WeekDay day="Monday" />
+                  <WeekDay day="Tuesday" />
+                  <WeekDay day="Wednesday" />
+                  <WeekDay day="Thursday" />
+                  <WeekDay day="Friday" />
+                  <WeekDay day="Saturday" />
+                  <WeekDay day="Sunday" />
+                </ul>
               </div>
-            </li>
-          </div>
+            </p>
+            <div className="accordion" id="accordionPanelsStayOpenExample">
+              <DailyMealPlanning mealid="Healthy-Fat">
+                <input
+                  className="inputmealplanner"
+                  placeholder="Add an item to your list"
+                ></input>
+              </DailyMealPlanning>
+              <DailyMealPlanning mealid="Protein">
+                <input
+                  className="inputmealplanner"
+                  placeholder="Add an item to your list"
+                ></input>
+              </DailyMealPlanning>
+              <DailyMealPlanning mealid="Dairy">
+                <input
+                  className="inputmealplanner"
+                  placeholder="Add an item to your list"
+                ></input>
+              </DailyMealPlanning>
+              <DailyMealPlanning mealid="Grains-Carbohydrates">
+                <input
+                  className="inputmealplanner"
+                  placeholder="Add an item to your list"
+                ></input>
+              </DailyMealPlanning>
+              <DailyMealPlanning mealid="Vegetables">
+                <input
+                  className="inputmealplanner"
+                  placeholder="Add an item to your list"
+                ></input>
+              </DailyMealPlanning>
+              <DailyMealPlanning mealid="Fruits">
+                <input
+                  className="inputmealplanner"
+                  placeholder="Add an item to your list"
+                ></input>
+              </DailyMealPlanning>
+            </div>
+          </li>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
 };
